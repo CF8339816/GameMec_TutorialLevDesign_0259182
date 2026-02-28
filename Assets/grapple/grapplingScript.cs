8b16bdf9d0cbe9349cff44b094b0ee1a87c8fe6e
CF8339816 <chris_french_3dga@hotmail.com>
2026-02-27 20:01:20 -0400

    Working on getting unity set up to iunterract with new grapplaer code
    set up debug logs and tweaked some of the code while debugging.

--- Assets/grapple/grapplingScript.cs
+++ Assets/grapple/grapplingScript.cs
@@ -1,55 +1,118 @@
-using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
-public class grapplingScript : MonoBehaviour
+using UnityEngine;
+
+public class PullGrapple : MonoBehaviour
 {
-  
     [Header("References")]
-    private PlayerMovementGrappling pm;
+    private PlayerController pm;
     public Transform cam;
-    public Transform gunTip;
+    public Transform grapplerTip;
     public LayerMask whatIsGrappleable;
+    public LineRenderer lr;
 
+    [Header("Pull Settings")]
+    public float maxGrappleDistance = 50f;
+    public float pullSpeed = 20f;
+    public float stopDistance = 2f; // Distance from player to stop pulling
+    public float grapplingCd = 1f;
 
-    [Header("Grappling")]
-    public float maxGrappleDistance;
-    public float grappleDelayTime;
-
-
-    private Vector3 grapplePoint;
-
-    [Header("Cooldown")]
-    public float grapplingCd;
+    private GameObject pulledObject;
+    private Rigidbody targetRb;
     private float grapplingCdTimer;
+    private bool isPulling;
 
     [Header("Input")]
     public KeyCode grappleKey = KeyCode.Mouse1;
 
-    private bool grappling;
+    public GameObject Grappler;
 
     private void Start()
     {
-        pm = GetComponent<PlayerMovementGrappling>();
+        pm = GetComponent<PlayerController>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(grappleKey) && grapplingCdTimer <= 0)
+        {
+            Grappler.SetActive(true);
+            StartGrapple();
+        }
+        if (isPulling) ExecutePull();
 
+        if (grapplingCdTimer > 0)
+            grapplingCdTimer -= Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        if (isPulling && pulledObject != null)
+        {
+            lr.enabled = true;
+            lr.SetPosition(0, grapplerTip.position);
+            lr.SetPosition(1, pulledObject.transform.position);
+        }
+        else
+        {
+            lr.enabled = false;
+        }
+    }
 
     private void StartGrapple()
     {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, whatIsGrappleable))
+        {
+            pulledObject = hit.collider.gameObject;
+            targetRb = pulledObject.GetComponent<Rigidbody>(); //  targets the raycast to the objects ridgid body
 
+           
+            if (targetRb != null)// prevenmts pulling objects with no ridgid body
+            {
+                isPulling = true;
+                targetRb.useGravity = false; // Disable gravity while pulling
+                targetRb.linearDamping = 0;   // Prevent air resistance from slowing it down
+            }
+        }
     }
 
-    private void ExecuteGrapple()
+    private void ExecutePull()
     {
+        if (pulledObject == null)
+        {
+            Grappler.SetActive(false);
+            StopGrapple();
+            return;
+        }
 
+        float distance = Vector3.Distance(grapplerTip.position, pulledObject.transform.position);
+
+        if (distance > stopDistance)
+        {
+           
+            Vector3 direction = (grapplerTip.position - pulledObject.transform.position).normalized; //sets direction of pull toward player
+
+           
+            targetRb.linearVelocity = direction * pullSpeed; //applies velocity to the pull
+        }
+        else
+        {
+            StopGrapple();
+        }
     }
 
     public void StopGrapple()
     {
+        isPulling = false;
 
+        if (targetRb != null)
+        {
+            targetRb.useGravity = true;
+            targetRb.linearVelocity = Vector3.zero; // Stop the object when it gets to you
+        }
+
+        pulledObject = null;
+        targetRb = null;
+        grapplingCdTimer = grapplingCd;
     }
-
-
-}
-}
 }
